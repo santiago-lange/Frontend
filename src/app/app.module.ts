eb9317fbd15b2cca1df5dfaabdda2e0e698faf35
santiago-lange <santiagolange2504@gmail.com>
2023-05-25 00:32:01 -0300

    Creacion componente login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { SkillsComponent } from './components/skills/skills.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import { HttpClientModule } from '@angular/common/http';
     AcercaDeComponent,
     ProjectsComponent,
     SkillsComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
